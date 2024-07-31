26076eae337e5444bf5ff28901ffca20ff109df5
Mario <marioromano1998@gmail.com>
2024-07-31 23:23:11 +0200

    Project Work v0.1
    First version of the project work.

---
+++ Project Work/script.js
@@ -0,0 +1,7 @@
+// Script JavaScript personalizzato
+document.addEventListener("DOMContentLoaded", function() {
+    // Questo evento viene eseguito quando il DOM è completamente caricato
+    console.log("DOM completamente caricato e analizzato");
+
+    // Puoi aggiungere qui il tuo codice JavaScript personalizzato
+});
